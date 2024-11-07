e090d9b514d52a8c8ce6188cada75264a58fffdf
colaola20 <soryo@farmingdale.edu>
2024-11-07 11:37:36 -0500

    Worked on Home Scene design. Did the Task details look, filtering bar, and "home" and "add" buttons.

--- src/main/java/org/example/todo_list/view_models/AppController.java
+++ src/main/java/org/example/todo_list/view_models/AppController.java
@@ -3,7 +3,13 @@ package org.example.todo_list.view_models;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.text.TextFlow;
 import org.example.todo_list.SceneManager;
 
 import java.net.URL;
@@ -14,12 +20,37 @@ public class AppController implements Initializable {
     SceneManager sceneManager = SceneManager.getInstance();
 
     @FXML
-    private Label welcomeText;
+    private Button personalTasksBtn, importantTasksBtn, homeStuffTasksBtn, addTaskBtn, allTasksBtn, criticalTasksBtn, daysTasksBtn, groupTasksBtn, homeBtn, monthTasksBtn, weekTasksBtn;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Label myTAsksLabel, calendarLabel, tagsLabel;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Menu menuBarCreate, menuBarEdit, menuBarFile, menuBarGroup, menuBarHelp;
+
+    @FXML
+    private BorderPane innerTaskBP, outerBP;
+
+    @FXML
+    private Label taskDueDate, taskName, taskPriority;
+
+    @FXML
+    private TextFlow taskDescription;
+
+    @FXML
+    private ProgressBar taskProgressBar;
+
+    @FXML
+    private Polygon taskTag;
+
+    @FXML
+    private VBox taskContainer;
 
     public void logOut(ActionEvent actionEvent) {
         sceneManager.showScene("LoginScene");
