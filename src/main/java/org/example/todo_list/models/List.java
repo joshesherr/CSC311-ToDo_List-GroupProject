eb7840b8fd51d31dfeb8c8ffdd7f37defcfd266b
selincs <selin.benan7@gmail.com>
2024-11-12 12:44:53 -0500

    Created skeleton of Home Page GUI for Personal Task Viewing. This view allows a user to view the personal tasks they have created. No controller attached to this View yet.

--- src/main/java/org/example/todo_list/models/List.java
+++ src/main/java/org/example/todo_list/models/List.java
@@ -11,6 +11,10 @@ public class List {
     private ArrayList<String> shareUsers;
     private PriorityQueue<Task> tasks;
     private float progress=0.0f;
+    private ArrayList<Tag> listTags = new ArrayList<>(); //This may actually be not needed here but in Task instead
+    // Create a default tag that each list will start with and be replaced?
+    //Or start empty? Only reason I wonder this is because of color setting in tags,
+    //and how we want to use Color
 
     public List() {
         this.title = "";
@@ -47,4 +51,11 @@ public class List {
         tasks.add(task);
     }
 
+    public ArrayList<Tag> getListTags() {
+        return listTags;
+    }
+
+    public void setListTags(ArrayList<Tag> listTags) {
+        this.listTags = listTags;
+    }
 }
--- src/main/java/org/example/todo_list/models/Tag.java
+++ src/main/java/org/example/todo_list/models/Tag.java
@@ -2,9 +2,46 @@ package org.example.todo_list.models;
 
 import javafx.scene.paint.Color;
 
+import java.util.Objects;
+
 public class Tag {
 
     private String name;
     private Color color;
 
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    //Equals currently checks for color, we should implement a way to guarantee users can easily match colors for
+    //lists and tasks in the same group
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (!(o instanceof Tag tag)) return false;
+        //Im not sure how to check equality of colors atm, this may need tweaking
+        return tag.getName().equalsIgnoreCase(this.name) && Objects.equals(color, tag.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, color);
+    }
 }
